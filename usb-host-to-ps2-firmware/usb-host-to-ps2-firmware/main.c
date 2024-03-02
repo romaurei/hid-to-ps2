c1a6d25b6fcfb6c3b30280c98aac7307aa574c6c
Rodrigo Maureira <rodrigo@rodri.tech>
2024-03-02 15:52:10 +1100

    Added UART code to main.c

--- usb-host-to-ps2-firmware/usb-host-to-ps2-firmware/main.c
+++ usb-host-to-ps2-firmware/usb-host-to-ps2-firmware/main.c
@@ -1,5 +1,81 @@
 #include <atmel_start.h>
 #include <hal_delay.h>
+#include <hpl_dma.h>
+
+// Buffer length to transfer/receive
+#define BUFFER_LEN 8
+
+// DMA channel numbers for receive and transmit
+#define USART_RX_DMA_CH 0
+#define USART_TX_DMA_CH 1
+
+static uint16_t string[BUFFER_LEN];
+
+// callbacks
+static void dma_transfer_done_rx(struct _dma_resource *const resource)
+{
+	_dma_enable_transaction(USART_TX_DMA_CH, false);
+}
+
+static void dma_error_rx(struct _dma_resource *const resource)
+{
+	// write error handling code here
+}
+
+static void dma_transfer_done_tx(struct _dma_resource *const resource)
+{
+	_dma_enable_transaction(USART_RX_DMA_CH, false);
+}
+
+static void dma_error_tx(struct _dma_resource *const resource)
+{
+	// write error handling code here
+}
+
+// register callbacks
+void Register_dma_rx_callback(void)
+{
+	struct _dma_resource *resource_rx;
+	_dma_get_channel_resource(&resource_rx, USART_RX_DMA_CH);
+	resource_rx->dma_cb.transfer_done = dma_transfer_done_rx;
+	resource_rx->dma_cb.error         = dma_error_rx;
+}
+
+void Register_dma_tx_callback(void)
+{
+	struct _dma_resource *resource_tx;
+	_dma_get_channel_resource(&resource_tx, USART_TX_DMA_CH);
+	resource_tx->dma_cb.transfer_done = dma_transfer_done_tx;
+	resource_tx->dma_cb.error         = dma_error_tx;
+}
+
+// USART RX channel configuration
+void Configure_Channel_rx()
+{
+	_dma_set_source_address(USART_RX_DMA_CH, (uint32_t) & (((Sercom *)(USART_0.device.hw))->USART.DATA.reg));
+	_dma_set_destination_address(USART_RX_DMA_CH, (uint32_t *)string);
+	_dma_set_data_amount(USART_RX_DMA_CH, (uint32_t)BUFFER_LEN);
+
+	// callback
+	Register_dma_rx_callback();
+
+	// Enable DMA transfer complete interrupt
+	_dma_set_irq_state(USART_RX_DMA_CH, DMA_TRANSFER_COMPLETE_CB, true);
+}
+
+// USART TX channel configuration
+void Configure_Channel_tx()
+{
+	_dma_set_source_address(USART_TX_DMA_CH, (uint32_t *)string);
+	_dma_set_destination_address(USART_TX_DMA_CH, (uint32_t) & (((Sercom *)(USART_0.device.hw))->USART.DATA.reg));
+	_dma_set_data_amount(USART_TX_DMA_CH, (uint32_t)BUFFER_LEN);
+
+	// callback
+	Register_dma_tx_callback();
+
+	// Enable DMA transfer complete interrupt
+	_dma_set_irq_state(USART_TX_DMA_CH, DMA_TRANSFER_COMPLETE_CB, true);
+}
 
 static void _mouse_move_cb(struct hidhf_mouse *mouse, int8_t x, int8_t y, int8_t scroll)
 {
@@ -11,6 +87,15 @@ int main(void)
 {
 	/* Initializes MCU, drivers and middleware */
 	atmel_start_init();
+	
+	// enable USART module
+	usart_sync_enable(&USART_0);
+
+	// Configure DMA channels
+	Configure_Channel_rx();
+	Configure_Channel_tx();
+	
+	_dma_enable_transaction(USART_RX_DMA_CH, false);
 
 	/* LED is turned on by polling buttons state */
 	/* LED is toggled by move callback */
